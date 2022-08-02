14996d0bc471d3e0c32c0a37fd1fc707c8af1115
Jakub Gil <263455@student.pwr.edu.pl>
2022-08-02 19:14:23 +0200

    Main game with 40 times reapet, prepared for neural network

--- main.py
+++ main.py
@@ -51,35 +51,63 @@ def show_stats(player_hand, croupier_hand):
 
 def main():
     #Setting up hands
-    new_deck = Deck()
-    croupier_hand = Hand(new_deck)
-    player_hand = Hand(new_deck)
+    for h in range(40):    
+        new_deck = Deck()
+        croupier_hand = Hand(new_deck)
+        player_hand = Hand(new_deck)
 
     #Main game
-    stays = False
-    while stays == False:
-        show_stats(player_hand=player_hand, croupier_hand=croupier_hand)
         
-        imp = input("Press \"D\" to draw or \"S\" to stay:")
-        if imp == "D" :
-            player_hand.draw(new_deck)
-        if imp == "S":
-            stays = True
+        lose = False
+        stays = False
+        while stays == False:
+            show_stats(player_hand=player_hand, croupier_hand=croupier_hand)
 
-    
+            if player_hand.sum_up() > 21:
+                print("------Busted!------")
+                stays = True
+                lose = True
+            
+            if lose != True:
+                imp = input("Press \"D\" to draw or \"S\" to stay:")
+                if imp == "D":
+                    player_hand.draw(new_deck)
+                
+                if imp == "S":
+                    stays = True
 
-    while croupier_hand.sum_up() <= player_hand.sum_up():
-        show_stats(player_hand=player_hand, croupier_hand=croupier_hand)
-
-        croupier_hand.draw(new_deck)
-        sleep(2)
-    
-    print("------Croupier hand------")
-    print(croupier_hand.cards)
-    print("Sum:{}".format(croupier_hand.sum_up()))
-
-    print("--------Your hand-------")
-    print(player_hand.cards)
-    print("Sum:{}".format(player_hand.sum_up()))     
         
+
+        while croupier_hand.sum_up() <= player_hand.sum_up():
+            print("-------------------------")         
+            print("------Croupier hand------")
+            print(croupier_hand.cards)
+            print("Sum:{}".format(croupier_hand.sum_up()))
+
+            print("--------Your hand-------")
+            print(player_hand.cards)
+            print("Sum:{}".format(player_hand.sum_up()))    
+            croupier_hand.draw(new_deck)
+            sleep(2)
+        
+        print("-------------------------")         
+        print("------Croupier hand------")
+        print(croupier_hand.cards)
+        print("Sum:{}".format(croupier_hand.sum_up()))
+
+        print("--------Your hand-------")
+        print(player_hand.cards)
+        print("Sum:{}".format(player_hand.sum_up()))     
+        
+        if lose == True:
+            print("-----------You lose!!!--------------")
+        
+        else:
+            if (player_hand.sum_up() <= 21) and (player_hand.sum_up() > croupier_hand.sum_up()) and (croupier_hand.sum_up() <= 21):
+                print("-----------Congratulations, You win!!!--------------")
+            elif croupier_hand.sum_up() > 21:
+                print("-----------Congratulations, You win!!!--------------")
+            else:
+                print("-----------You lose!!!--------------")        
+
 main()
